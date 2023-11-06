d6187b6b1e09d4df1762265ee77618f7f1126f4d
Mikatzu <mr.frienk@gmail.com>
2023-11-06 23:04:03 +0600

    knowledge updated: components

---
+++ src/components/Image.js
@@ -0,0 +1,11 @@
+import React from "react";
+
+class Image extends React.Component {
+    render(){ // wewew
+        return(
+                <img src={this.props.image}/>
+        )
+    }
+}
+
+export default Image
