acb73db6f9de04c64e04912af80779f813906665
Mikatzu <mr.frienk@gmail.com>
2023-11-16 17:51:32 +0600

    Теперь боди надо делать

--- src/App.js
+++ src/App.js
@@ -1,21 +1,16 @@
 import React from "react"
 import Header from "./components/Header"
-import Image from "./components/Image"
-import logo from "./svg/sk_granit_logo.svg"
+import Main from "./components/Main"
+
 class App extends React.Component {
-    helpText = "Help text"
     render() {
-        return (<div className="name">
-            <Header title="hello sss" />
-            <h1>{this.helpText}</h1>
-            <input placeholder={this.helpText}
-                   onClick={this.inputClick} onMouseEnter={this.mouseOver}/>
-            <p>{this.helpText === "Help text!" ? "Yes" : "No"}</p>
-            <Image image={logo} />
-        </div>)
+        return (
+            <div className="MainPage">
+            <Header/>
+            <div/>
+            </div>
+        )
     }
-    inputClick(){console.log("Clicked")}
-    mouseOver(){console.log("Mouse Over")}
 }
 
 export default App
--- src/components/Header.js
+++ src/components/Header.js
@@ -1,9 +1,20 @@
 import React from "react";
+import logo from "../../src/svg/sk_granit_logo.svg"
+import callingLogo from "../../src/svg/calling.svg"
 
 class Header extends React.Component {
     render(){
         return(
-            <header className="header">{this.props.title}</header>
+            <header className="header">
+                <img src={logo} alt=""/>
+                <a className="headerLinks" href="">УСЛУГИ</a>
+                <a className="headerLinks" href="">КАТАЛОГ</a>
+                <a className="headerLinks" href="">СПЕЦ.ТЕХНИКА</a>
+                <a className="headerLinks" href="">НАШИ РАБОТЫ</a>
+                <a className="headerLinks" href="">О КОМПАНИИ</a>
+                <a className="headerLinks" href="">КОНТАКТЫ</a>
+                <p className="headerHotlineNumber"><img className="hotlineCallingLogo" src={callingLogo} alt=""/>+7 (999) 000 - 00 - 00</p>
+            </header>
         )
     }
 }
